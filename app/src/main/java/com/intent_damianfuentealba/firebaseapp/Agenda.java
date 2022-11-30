4d8b0ea8132606704758890a0c0d553151be503f
IDG9I <damiangamerxd123@gmail.com>
2022-11-30 17:50:31 -0300

    LOL errores en el archivo

---
+++ app/src/main/java/com/intent_damianfuentealba/firebaseapp/Agenda.java
@@ -0,0 +1,56 @@
+package com.intent_damianfuentealba.firebaseapp;
+
+public class Agenda {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    public Agenda() {
+    }
+
+    public Agenda(int id) {
+        this.id = id;
+    }
+
+    public Agenda(int id, String nombre, String telefono, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "agenda{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+}
